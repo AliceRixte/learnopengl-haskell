aa5ab66f3f215185948396ea466b7d6ab7fccb1c
Alice Rixte <alice.rixte@gmail.com>
2024-10-18 20:41:28 +0200

    Fix hellotriangle

--- app/hellotriangle/Main.hs
+++ app/hellotriangle/Main.hs
@@ -41,7 +41,7 @@ main = do
     SDL.createWindow
       "SDL / OpenGL Example"
       SDL.defaultWindow {SDL.windowInitialSize = V2 screenWidth screenHeight,
-                         SDL.windowGraphicsContext = SDL.defaultOpenGL}
+                         SDL.windowGraphicsContext = SDL.OpenGLContext SDL.defaultOpenGL}
   SDL.showWindow window
 
   _ <- SDL.glCreateContext window
